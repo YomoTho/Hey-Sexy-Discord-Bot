d0482a4ca30419bddb2bdd065f80bb3dd8b0aa29
YomoTho <vrekmakerq@gmail.com>
2021-03-31 22:39:22 +0200

    Revert "Bug fixed. Make it so bot will check bot first than player 1"
    This reverts commit 618fc4f613cba97b3a0246c01975d8f7e46eb879.

--- scripts/games.py
+++ scripts/games.py
@@ -69,10 +69,8 @@ class TicTacToe:
 
     async def smart_bot_move(self):
         move = None
-
-        _other_player = self.player_2 if self.turn == self.player_1 else self.player_2
-
-        for p in [self.turn, _other_player]:
+        other_player = self.player_2 if self.turn == self.player_1 else self.player_1
+        for p in [self.turn, other_player]:
             ox = self.o if p == self.player_1 else self.x
             for i in self.reactions: # self.reactions is like all the possible moves
                 gameBoard_copy = self.gameBoard.copy()
