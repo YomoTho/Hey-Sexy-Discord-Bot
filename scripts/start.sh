fd4fa01729a057a43f007e19d0a58b2ced385399
YomoTho <vrekmakerq@gmail.com>
2021-05-01 14:13:47 +0200

    Added 'Server Mode' in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -24,17 +24,33 @@ function cleanup()
 
 function main() 
 {
-    cleanup $1
-    while [[ : ]]
-    do
-        python3 main.py 1 2> $(p) && break
-        echo > $(p "r")
-        code=$(rw $(p))
-        [[ $(echo $code | cut -c1-3) != "1::" ]] && break
-        echo $code > $(p "r")
-        echo "Rebooting..."
-    done
-    cleanup $1
+    if [[ $1 == "-s" ]]; then
+        cleanup $1
+        echo "Server Mode"
+        while [[ : ]]
+        do
+            python3 main.py 1 2> $(p)
+            echo > $(p "r")
+            code=$(rw $(p))
+            echo $code > $(p "r")
+            echo "Rebooting..."
+            sleep 1
+        done
+        cleanup $1
+    else
+        cleanup $1
+        echo "Normal Mode."
+        while [[ : ]]
+        do
+            python3 main.py 1 2> $(p) && break
+            echo > $(p "r")
+            code=$(rw $(p))
+            [[ $(echo $code | cut -c1-3) != "1::" ]] && break
+            echo $code > $(p "r")
+            echo "Rebooting..."
+        done
+        cleanup $1
+    fi
 }
 
 clear
