016b633e50c6bea5bd93a2fdc88109b8568241b8
YomoTho <74270998+YomoTho@users.noreply.github.com>
2021-06-30 01:43:47 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,13 +3,21 @@
 function main()
 {
     clear
-    while [ : ]
-    do
-        reboot_id=$(cat reboot_id)
-        echo "" > reboot_id
-        python3 scripts/main.py $reboot_id
-        sleep 2
-    done
+
+    if [[ $1 == 'loop' ]]; then
+        echo "Server Mode."
+        while [ : ]
+        do
+            reboot_id=$(cat reboot_id)
+            echo "" > reboot_id
+            python3 scripts/main.py $reboot_id
+            sleep 1
+        done
+    else
+        echo "Normal Mode."
+        python3 scripts/main.py
+    fi
+
 }
 
-main
+main $@
