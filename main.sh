fc6f5b48fb62c83e3e1032592b566bda8917a891
YomoTho <74270998+YomoTho@users.noreply.github.com>
2021-07-21 01:28:05 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,15 +4,19 @@ function main()
 {
     clear
 
-    if [[ $1 == 'loop' ]]; then
-        echo "Server Mode."
-        while [ : ]
+    if [[ $1 == 'loop' || $1 == '-l' ]]; then
+        echo "Always looping."
+        while [[ : ]]
         do
             reboot_id=$(cat reboot_id)
-            echo "" > reboot_id
+            /dev/null > reboot_id
             python3 scripts/main.py $reboot_id
-            # sleep 1
+            sleep 0.5
         done
+    elif [[ $1 == 'errors' || $1 == '-e' ]]; then
+        echo "Show errors only"
+
+        python3 scripts/main.py 1> /dev/null 
     else
         echo "Normal Mode."
         python3 scripts/main.py
